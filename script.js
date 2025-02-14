b75229e27c3a527c8c88e90b41389b558e25b4f6
Husain <183328856+Husain-020@users.noreply.github.com>
2025-02-13 20:21:45 -0500

    Created getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("test")
+function getComputerChoice(){
+    let choice = Math.random()
+
+    if (choice <= 0.99/3){
+        return "rock"
+    } else if (choice > 0.99/3 && choice <= (0.99 * 2)/3){
+        return "paper"
+    } else {
+        return "scissor"
+    }
+}
+
+console.log(getComputerChoice())
