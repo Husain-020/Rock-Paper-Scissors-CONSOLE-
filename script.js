5c6b726e9f7b1ed15534bdefe96a0c117b3bdc12
Husain <183328856+Husain-020@users.noreply.github.com>
2025-03-12 18:44:44 -0400

    Display score, outcome and both player and computer's decision into UI.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const options = document.querySelector("#options")
+const accounts = document.querySelector("#accounts")
+
+const playerScoreDisplay = document.querySelector("#playerScore")
+const computerScoreDisplay = document.querySelector("#computerScore")
+
+const playerChoiceImg = document.querySelector("#playerChoiceImg")
+const computerChoiceImg = document.querySelector("#computerChoiceImg")
+
+const outcomeDisplay = document.querySelector("#outcome")
+
+const rockImg = "assets/icons/rock.webp"
+const paperImg = "assets/icons/paper.jpg"
+const scissorImg = "assets/icons/scissors.avif"
+
+let playerScore = 0
 let computerScore = 0
-let humanScore = 0
 
 function finishGame(winner){
     const buttons = document.querySelectorAll("button")
@@ -12,7 +27,6 @@ function finishGame(winner){
     finishMessage.textContent = `${winner} won the game!`
     finishMessage.style.fontSize = "50px";
 
-    const options = document.querySelector("#options")
     options.appendChild(finishMessage)
 }
 
@@ -30,18 +44,22 @@ function getComputerChoice(){
 
 function playRound(humanChoice, computerChoice){
     if (humanChoice === "rock"){
+        playerChoiceImg.src = rockImg
+
         if (computerChoice === "rock"){
            return `Draw!`
         } else if (computerChoice === "paper") {
             computerScore += 1
             return "Computer wins!"
         } else {
-            humanScore += 1
+            playerScore += 1
             return "You win!"
         }
     } else if (humanChoice === "paper") {
+        playerChoiceImg.src = paperImg
+
         if (computerChoice === "rock"){
-            humanScore += 1
+            playerScore += 1
             return "You win!"
         } else if (computerChoice === "paper") {
             return "Draw!"
@@ -50,11 +68,13 @@ function playRound(humanChoice, computerChoice){
             return "Computer wins!"
         }
     } else if (humanChoice === "scissor") {
+        playerChoiceImg.src = scissorImg
+
         if (computerChoice === "rock"){
             computerScore += 1
             return "Computer wins!"
         } else if (computerChoice === "paper") {
-            humanScore += 1
+            playerScore += 1
             return "You win!"
         } else {
             return "Draw!"
@@ -68,8 +88,12 @@ function playGame(humanChoice){
     
         let outcome = playRound(humanChoice, computerChoice)
         
-        console.log(`Player: ${humanChoice}\nComputer: ${computerChoice}\n"${outcome}"\nPlayer: ${humanScore}\nComputer: ${computerScore}`)
+        outcomeDisplay.querySelector("h1").textContent = `${outcome}`
 
+        console.log(`Player: ${humanChoice}\nComputer: ${computerChoice}\n"${outcome}"\nPlayer: ${playerScore}\nComputer: ${computerScore}`)
+    
+        playerScoreDisplay.textContent = `${playerScore}/3`
+        computerScoreDisplay.textContent = `${computerScore}/3`
     } else {
         console.log(humanChoice)
     }
@@ -83,3 +107,6 @@ buttons.forEach((button) => {
         playGame(humanChoice)
     })
 })
+
+// Make accounts div max-height
+accounts.style.maxHeight = accounts.offsetHeight + "px"
